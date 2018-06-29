e8e61241f383e457782ed6bb070b77e525e6a878
mjcampagna <mjcampagna@gmail.com>
2018-06-29 16:39:38 -0700

    Tests GET, POST, DELETE methods

--- databases/cassandra/index.js
+++ databases/cassandra/index.js
@@ -4,14 +4,36 @@ const client = new cassandra.Client({
 	keyspace: 'cava'
 });
 
-const getAllReviews = function(restaurantId, callback) {
+const postReview = function(post, callback) {
+	const query = `INSERT INTO reviews (id,restaurant_id,username,date,overall_rating,food_rating,service_rating,ambiance_rating,value_rating,noise_level,recommended,body) VALUES (${post.id},${post.restaurant_id},'${post.username}','${post.date}',${post.overall_rating},${post.food_rating},${post.service_rating},${post.ambiance_rating},${post.value_rating},${post.noise_level},${post.recommended},'${post.body}');
+	`;
+	client.execute(query)
+		.then( res => {
+			callback(null, res);
+		})
+		.catch( err => console.log(err));
+}
+
+const getReviews = function(restaurantId, callback) {
 	const query = `SELECT * FROM cava.reviews WHERE restaurant_id = ${restaurantId}`;
 	client.execute(query)
 		.then( res => {
 			callback(null, res);
-		});
+		})
+		.catch( err => console.log(err));
+}
+
+const deleteReview = function(restaurantId, callback) {
+	const query = `DELETE FROM reviews WHERE restaurant_id = ${restaurantId}`;
+	client.execute(query)
+		.then( res => {
+			callback(null, res);
+		})
+		.catch( err => console.log(err));
 }
 
 module.exports = {
-  getAllReviews
+	postReview,
+	getReviews,
+	deleteReview
 };
